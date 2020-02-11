db8ec57ca66272d485209dab8587331971e8e51e
jm-devweb <jm.noel.devweb@gmail.com>
2020-02-11 21:15:57 +0100

    first back

---
+++ back/src/main/java/co/simplon/outtogether/OuttogetherApplication.java
@@ -0,0 +1,13 @@
+package co.simplon.outtogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OuttogetherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run (OuttogetherApplication.class, args);
+    }
+
+}
---
+++ back/src/test/java/co/simplon/outtogether/OuttogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package co.simplon.outtogether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OuttogetherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
